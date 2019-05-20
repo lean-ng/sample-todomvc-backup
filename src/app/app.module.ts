9eae23e6a199c5c44fae33dafbc39eb9409759f4
Micha Alt <mail@lean-stack.de>
2019-05-20 20:41:50 +0200

    ng: todo app shell created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoShellComponent } from './components/todo-shell/todo-shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoShellComponent
   ],
   imports: [
     BrowserModule
