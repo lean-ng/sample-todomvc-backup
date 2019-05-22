8d320d81860d7530b800f77931071bc86d054a1e
Micha Alt <mail@lean-stack.de>
2019-05-22 19:35:01 +0200

    Injecting the state into the shell.

--- src/app/components/todo-shell/todo-shell.component.ts
+++ src/app/components/todo-shell/todo-shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   selector: 'todo-shell',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoShellComponent implements OnInit {
 
-  constructor() { }
+  // Vorsicht:
+  // Falls der state ´privat´ injeziert wird und die todos-Property
+  // auf eine Instanz-Variable kopiert wird (Referenz!), muss jede
+  // State-Manipulation (Löschen, Ändern, Neues Todo) das Original-Array
+  // mutieren und keinesfalls neu setzen auf dem State (zum Beispiel beim
+  // Löschen über die filter-Methode).
+  //
+  // Da ich insgesamt eine Immutable-Strategie schon jetzt anstrebe ist die
+  // bessere Option die public-Injektion und das weitergeben der todos-Property
+  // ohne lokale Kopie der Referenz
+
+  constructor(public state: StateService) { }
 
   ngOnInit() {
   }
