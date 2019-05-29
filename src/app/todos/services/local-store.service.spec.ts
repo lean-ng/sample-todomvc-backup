291dea1426e89d28350cc0b156408508358b11c2
Micha Alt <mail@lean-stack.de>
2019-05-30 00:36:01 +0200

    Local store service implemented and tested.

--- src/app/todos/services/local-store.service.spec.ts
+++ src/app/todos/services/local-store.service.spec.ts
@@ -1,17 +1,74 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 
 import { LocalStoreService } from './local-store.service';
+import { Todo } from '../models/todo';
 
 describe('LocalStoreService', () => {
 
   const storage = {};
 
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [{ provide: Storage, useValue: storage }]
+    providers: [{ provide: Storage, useFactory: () => storage }]
   }));
 
+  let store: LocalStoreService;
+  let firstTodo: Todo;
+  let secondTodo: Todo;
+
   it('should be created', () => {
-    const service: LocalStoreService = TestBed.get(LocalStoreService);
-    expect(service).toBeTruthy();
+    const storeService: LocalStoreService = TestBed.get(LocalStoreService);
+    expect(storeService).toBeTruthy();
+
+    // sichern für Test-Variante 3
+    store = storeService;
+  });
+
+  // Test-Variante 1:
+  // - Service vom Container anfordern
+  // - asynchrones Testen mit jasmine done
+  it('should initially have zero todos', (done) => {
+    const storeService: LocalStoreService = TestBed.get(LocalStoreService);
+    storeService.getAll().then(todos => {
+      expect(todos.length).toBe(0);
+      done();
+    });
+  });
+
+  // Test-Variante 2:
+  // - Service über inject-Helper injezieren
+  // - asynchrones Testen mit async-Helper
+  it('shoud create a first todo', async(inject([LocalStoreService], (storeService: LocalStoreService) => {
+    storeService.create('First Todo').then( todo => {
+      expect(todo.title).toBe('First Todo');
+      expect(todo.completed).toBeFalsy();
+      firstTodo = todo;
+    });
+  })));
+
+  // Test-Variante 3:
+  // - Service über lokale Variable übernehmen
+  // - asynchrones Testen mit async/await
+  it('should add a second todo', async () => {
+    secondTodo = await store.create('Second Todo');
+    const todos = await store.getAll();
+    expect(todos.length).toBe(2);
+  });
+
+  it('should update a todo', async () => {
+
+    const firstUpdatedTodo = await store.update(firstTodo.id, { completed: !firstTodo.completed});
+    expect(firstUpdatedTodo.completed).toBeTruthy();
+    const secondUpdatedTodo = await store.update(secondTodo.id, { title: 'Updated' });
+    expect(secondUpdatedTodo.title).toBe('Updated');
+
+    const todos = await store.getAll();
+    expect(todos[0]).toEqual(firstUpdatedTodo);
+    expect(todos[1]).toEqual(secondUpdatedTodo);
+  });
+
+  it('should delete a todo', async() => {
+    await store.remove(firstTodo.id);
+    const todos = await store.getAll();
+    expect(todos.length).toBe(1);
   });
 });
--- src/app/todos/services/local-store.service.ts
+++ src/app/todos/services/local-store.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Store } from './interfaces/store.interface';
+import { Todo } from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LocalStoreService {
+export class LocalStoreService implements Store{
 
-  constructor(private storage: Storage) { }
+  constructor(public storage: Storage) { }
+
+  async getAll(): Promise<Todo[]> {
+    return this.loadTodos();
+  }
+
+  async create(title: string): Promise<Todo> {
+    const newTodo: Todo = { id: this.generateId(), title, completed: false };
+
+    const todos  = this.loadTodos();
+    todos.push(newTodo);
+    this.saveTodos(todos);
+
+    return newTodo;
+  }
+
+  async update(id: number, changes: { title?: string; completed?: boolean; }): Promise<Todo> {
+    const todos = this.loadTodos();
+
+    const todo = todos.find(t => t.id === id);
+    Object.assign(todo, changes);
+    this.saveTodos(todos);
+
+    return todo;
+  }
+
+  async remove(id: number): Promise<void> {
+    const todos = this.loadTodos();
+
+    const ix = todos.findIndex(t => t.id === id);
+    todos.splice(ix, 1);
+    this.saveTodos(todos);
+  }
+
+  private loadTodos(): Todo[] {
+    return JSON.parse(this.storage.todos || '[]');
+  }
+
+  private saveTodos(todos: Todo[]) {
+    this.storage.todos = JSON.stringify(todos);
+  }
+
+  private generateId(): number {
+    let id = JSON.parse(this.storage.nextId || '1');
+    this.storage.nextId = id + 1;
+    return id;
+  }
 }
